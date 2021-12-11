6a4106f346d322750f962ba22dc39904a4d191c5
gamxu <wuttipat.did@gmail.com>
2021-12-11 12:17:21 +0700

    lab10_2 completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,7 +1,21 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+int printO(int row, int amount)
+{
+	if(row<=0 || amount<=0){
+		cout << "Invalid input\n";
+	}else{ 
+		for(int i=0; i<row; i++){
+			for(int j=0; j<amount; j++){
+				cout << "O";
+			}
+			cout << '\n';
+		}
+	}
+	return 0;
+}
+
 
 int main(){
 	
